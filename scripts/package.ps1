5ccf128821e975de113054024149bd16904cbccb
djfdyuruiry <djfdyuruiry@noreply.github.com>
2021-07-12 02:21:44 +0100

    Adding tests and packaging script

---
+++ scripts/package.ps1
@@ -0,0 +1,37 @@
+param (
+  $Version = "GIT_COMMIT"
+)
+
+$buildPath = "GoFigure.App\bin\Release"
+
+function Remove-Build-Items() {
+  param ($Filter)
+
+  Get-ChildItem $buildPath -Filter $Filter | ForEach-Object { Remove-Item -Force $_ }
+}
+
+function Main() {
+  if ($Version -eq "GIT_COMMIT") {
+    $Version = git rev-parse --short HEAD
+  }
+
+  Write-Host "Packaging 'Go Figure!' - Version=${Version}"
+
+  Write-Host "Cleaning build output"
+
+  Remove-Build-Items "*.pdb"
+  Remove-Build-Items "*.zip"
+  Remove-Build-Items "*.xml"
+  Remove-Build-Items "*.exe.config"
+
+  $outputPath = Join-Path $buildPath "go-figure-${Version}.zip"
+  
+  Write-Host "Building zip package @ $outputPath"
+
+  Compress-Archive "${buildPath}/*" -DestinationPath $outputPath
+
+  Write-Host "Packaging complete"
+}
+
+Main
+
